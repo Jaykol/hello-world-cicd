0ebf5922b506a390f4aca302c0115c74720a89ea
Jesutofunmi <jekola.ajekola@gmail.com>
2025-05-09 11:03:30 +0100

    add test_app.py and updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
+#hello world app
 
-
-def hello_word();
+def hello_word():
     return "Hello, World"
 
-    if_name_=="_main_":
-        print(hello_word)
+    if_name_=="_main_";
+    print(hello_word)
